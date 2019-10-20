0d4074bc5ffdbb8d7fcf1429c6ab2e70e82112e7
feernandobraga <fernando.bragga@gmail.com>
2019-10-20 19:41:38 +1100

    DAOs finished

--- src/blogpackage/model/dao/AdminDAO.java
+++ src/blogpackage/model/dao/AdminDAO.java
@@ -10,4 +10,25 @@ public class AdminDAO {
     private String DBURL = "jdbc:mysql://localhost:3306/EmployeeDB?serverTimezone=Australia/Sydney";
     private String DBUsername = "root";
     private String DBPassword = "mysql123";
+
+    // constructor
+    public AdminDAO() {
+    }
+
+    // Creates a connection to the database
+    protected Connection getConnection(){
+        Connection connection = null;
+        System.out.println("DAO AdminDAO- getConnection()"); //debugging
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(DBURL, DBUsername, DBPassword);
+        } catch (SQLException e){
+            e.printStackTrace();
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        return connection;
+    } // end getConnection
 }
--- src/blogpackage/model/dao/CategoryDAO.java
+++ src/blogpackage/model/dao/CategoryDAO.java
@@ -10,4 +10,27 @@ public class CategoryDAO {
     private String DBURL = "jdbc:mysql://localhost:3306/EmployeeDB?serverTimezone=Australia/Sydney";
     private String DBUsername = "root";
     private String DBPassword = "mysql123";
+
+    // constructor
+    public CategoryDAO() {
+    }
+
+    // Creates a connection to the database
+    protected Connection getConnection(){
+        Connection connection = null;
+        System.out.println("DAO CategoryDAO - getConnection()"); //debugging
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(DBURL, DBUsername, DBPassword);
+        } catch (SQLException e){
+            e.printStackTrace();
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        return connection;
+    } // end getConnection
+
+
 }
