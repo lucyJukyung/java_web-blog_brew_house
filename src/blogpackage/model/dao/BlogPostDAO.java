7a3da82cef7ccebfe13e6fd1a0e5f2fad53558dd
feernandobraga <fernando.bragga@gmail.com>
2019-10-20 22:52:28 +1100

    BlogPostDAO - selectAllPosts()

--- src/blogpackage/model/dao/BlogPostDAO.java
+++ src/blogpackage/model/dao/BlogPostDAO.java
@@ -7,13 +7,21 @@ import java.util.*;
 public class BlogPostDAO {
 
     //defining instance variables
-    private String DBURL = "jdbc:mysql://localhost:3306/EmployeeDB?serverTimezone=Australia/Sydney";
+    private String DBURL = "jdbc:mysql://localhost:3306/BlogDB?serverTimezone=Australia/Sydney";
     private String DBUsername = "root";
     private String DBPassword = "mysql123";
 
     // constructor
     public BlogPostDAO() {
     }
+
+    //--------------------------
+    //      SQL QUERIES
+    //--------------------------
+    // retireve all posts from DB
+    private String SELECTALLPOSTS = "select * from post";
+
+
 
     // Creates a connection to the database
     protected Connection getConnection(){
@@ -31,6 +39,63 @@ public class BlogPostDAO {
 
         return connection;
     } // end getConnection
+
+
+    // retrieve all posts and store it on an array list <BlogPost>
+    public List <BlogPost> selectAllPosts(){
+        System.out.println("BlogPostDAO - selectAllPosts"); //debugging
+
+        // create an List Array "blogPosts" to store objects of the type BlogPost (java bean)
+        List <BlogPost> blogPosts = new ArrayList<>();
+
+        //initialize DB variables
+        Connection connection = null;
+        PreparedStatement preparedStatement =  null;
+        ResultSet rs = null;
+
+        //stablishing a connection
+        try {
+            // 1. step 1, connection
+            connection = getConnection();
+
+            // 2. step 2, set the prepared statement
+            preparedStatement = connection.prepareStatement(SELECTALLPOSTS);
+            System.out.println("BlogPostDAO - Running query: " + preparedStatement);
+
+            // 3. step 3, execute the query using the prepared statement
+            rs = preparedStatement.executeQuery();
+
+            // 4. step 4, iterating through the ResultSet and adding values to the array
+            while (rs.next()){
+                int postID = rs.getInt("postId");
+                String postTitle = rs.getString("postTitle");
+                Timestamp postDate = rs.getTimestamp("postDate");
+                String postAuthor = rs.getString("postAuthor");
+                String postContent = rs.getString("postContent");
+                Boolean isPostVisible = rs.getBoolean("postVisible");
+                int categoryID = rs.getInt("categoryID");
+
+                // create an new object of type BlogPost and adds it to the list array <BlogPosts>
+                blogPosts.add(new BlogPost(postID, postTitle, postDate, postAuthor, postContent, isPostVisible, categoryID));
+
+            } // end while
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            finallySQLException(connection,preparedStatement,rs);
+        } //end try/catch/finally
+
+        return blogPosts;
+
+    } // end selectAllPosts
+
+
+
+
+
+
+
 
     //method to close DB connection
     private void finallySQLException(Connection c, PreparedStatement p, ResultSet r) {
